32a30d8fa4aa5efbe7d6c40b10b3a50737876aeb
Zoey <3598851289@qq.com>
2026-08-27 21:38:10 +0800

    feat: expose desktop preferences in expanded view

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
+import { DesktopQuickControls } from './components/DesktopQuickControls';
 import { initializeDesktopLifecycle } from './services/desktopLifecycle';
 import './styles/app.css';
 import './styles/timer.css';
+import './styles/desktop.css';
 
 void initializeDesktopLifecycle().catch((error) => {
   console.error('Dayforge desktop lifecycle initialization failed:', error);
@@ -12,5 +14,6 @@ void initializeDesktopLifecycle().catch((error) => {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <DesktopQuickControls />
   </React.StrictMode>,
 );
