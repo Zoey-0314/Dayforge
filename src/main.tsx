f15353c964cf27667d314d417cf43683ccf444f1
Zoey <3598851289@qq.com>
2026-08-28 00:44:59 +0800

    fix: remove overlapping expanded quick controls

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
-import { DesktopQuickControls } from './components/DesktopQuickControls';
 import { initializeDesktopLifecycle } from './services/desktopLifecycle';
 import './styles/app.css';
 import './styles/timer.css';
 import './styles/desktop.css';
+import './styles/window-fixes.css';
 
 void initializeDesktopLifecycle().catch((error) => {
   console.error('Dayforge desktop lifecycle initialization failed:', error);
@@ -14,6 +14,5 @@ void initializeDesktopLifecycle().catch((error) => {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <DesktopQuickControls />
   </React.StrictMode>,
 );
